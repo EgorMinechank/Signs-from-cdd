55a34840a59f1abd504b853fa68da701ce8874c6
EgorMinechank <egypt_egor@mail.ru>
2025-05-11 15:29:55 +0300

    Initial commit

---
+++ src/main/java/net/em/sfc/SignsFromCddDataGenerator.java
@@ -0,0 +1,11 @@
+package net.em.sfc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SignsFromCddDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
