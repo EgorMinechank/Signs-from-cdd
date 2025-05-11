ced7067af7dcb39b64bb95dfa75290fad696d705
EgorMinechank <egypt_egor@mail.ru>
2025-05-11 16:29:49 +0300

    Someone changes

---
+++ src/main/java/net/em/sfc/SignsFromCddClient.java
@@ -0,0 +1,10 @@
+package net.em.sfc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SignsFromCddClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
